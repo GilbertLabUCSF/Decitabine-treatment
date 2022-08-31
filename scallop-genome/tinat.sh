499559b0714c41cae687fcd70df7d78b2c42fa6e
abearab <abardesigner@gmail.com>
2022-08-30 21:46:35 -0700

    redo salmon with updated scallop genome

--- scallop-genome/tinat.sh
+++ scallop-genome/tinat.sh
@@ -1,21 +1,51 @@
+# https://github.com/Kingsford-Group/scallop
 mkdir -p scallop/tinat/compare
 
-PATH=$PATH:/rumi/shams/abe/Workflows/rnaseqtools-1.0.3/bin/
+# export PATH='/sadra/goodarzilab/abe/Workflows/rnaseqtools-1.0.3/bin/':$PATH
 
 conda activate alignment
+cd scallop-genome/
 
-# 1. merge
-gtfmerge union scallop/tinat/tinat-gtf-list.txt scallop/tinat/hg19.tinat.gtf -t 18
-# 2. compare 
-gffcompare -o scallop/tinat/gffall -r /rumi/shams/genomes/hg19/hg19_genes.gtf scallop/tinat/hg19.tinat.gtf 
-# 3. subset
-gtfcuff puniq scallop/tinat/gffall.hg19.tinat.gtf.tmap scallop/tinat/hg19.tinat.gtf /rumi/shams/genomes/hg19/hg19_genes.gtf scallop/tinat/unique.gtf
-# 4. gtf2fasta
-gffread scallop/tinat/unique.gtf -g /rumi/shams/genomes/hg19/hg19.fa -w scallop/tinat/unique.fa
-# 5.concatenate with hg19 fasta
-# mkdir -p scallop/hg19
-# wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/hg19.knownGene.gtf.gz
-# mv hg19.knownGene.gtf.gz scallop/hg19/hg19.knownGene.gtf.gz
-# gunzip scallop/hg19/hg19.knownGene.gtf.gz
-gffread scallop/hg19/hg19.knownGene.gtf -g /rumi/shams/genomes/hg19/hg19.fa -w scallop/hg19/hg19.knownGene.fa
-cat scallop/hg19/hg19.knownGene.fa scallop/tinat/unique.fa > scallop/tinat/hg19.tinat.fa
+### 0. download genomes
+# # mkdir -p hg19
+# # wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/hg19.knownGene.gtf.gz
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/GRCh37_mapping/gencode.v34lift37.basic.annotation.gtf.gz
+gunzip gencode.v34lift37.basic.annotation.gtf.gz
+# # mv hg19.knownGene.gtf.gz hg19/hg19.knownGene.gtf.gz
+# # gunzip hg19/hg19.knownGene.gtf.gz
+
+### 1. merge
+gtfmerge union gtf-list.txt DAC_GRCh37_merged.gtf -t 2
+
+# ### 2. compare 
+# gffcompare -o hg19_tinat/gffall -r /rumi/shams/genomes/hg19/hg19_genes.gtf tinat/hg19.tinat.gtf 
+
+# ### 3. subset
+# gtfcuff puniq \
+#     tinat/gffall.hg19.tinat.gtf.tmap \
+#     tinat/hg19.tinat.gtf \
+#     /rumi/shams/genomes/hg19/hg19_genes.gtf \
+#     tinat/unique.gtf
+
+### 4. gtf2fasta
+# gffread tinat/unique.gtf -g /rumi/shams/genomes/hg19/hg19.fa -w tinat/unique.fa
+gffread DAC_GRCh37_merged.gtf -g /rumi/shams/genomes/hg19/hg19.fa -w DAC_GRCh37_merged.fa
+
+# # 5.concatenate with hg19 fasta
+# gffread hg19/hg19.knownGene.gtf -g /rumi/shams/genomes/hg19/hg19.fa -w hg19/hg19.knownGene.fa
+# cat hg19/hg19.knownGene.fa tinat/unique.fa > tinat/hg19.tinat.fa
+
+### aligner index 
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/GRCh37_mapping/GRCh37.primary_assembly.genome.fa.gz
+gunzip GRCh37.primary_assembly.genome.fa.gz
+
+mkdir STAR.index
+
+STAR --runThreadN 6 \
+    --runMode genomeGenerate \
+    --genomeDir STAR.index \
+    --genomeFastaFiles GRCh37.primary_assembly.genome.fa \
+    --sjdbGTFfile DAC_GRCh37_merged.gtf \
+    --sjdbOverhang 99
+
+rm GRCh37.primary_assembly.genome.fa.gz
