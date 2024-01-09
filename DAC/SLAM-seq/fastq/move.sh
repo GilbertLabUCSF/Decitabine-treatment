03e8853923dfe1faae2e20e39398d68f45417272
abearab <abardesigner@gmail.com>
2024-01-09 10:29:07 -0800

    download scripts

---
+++ DAC/SLAM-seq/fastq/move.sh
@@ -0,0 +1,26 @@
+
+curl --netrc -O ftps://ftp.box.com/AGLG06/0_t_1_S46_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/0_t_2_S47_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/0_t_3_S48_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/0_u_1_S43_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/0_u_2_S44_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/0_u_3_S45_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/2_t_1_S52_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/2_t_2_S53_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/2_t_3_S54_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/2_u_1_S49_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/2_u_2_S50_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/2_u_3_S51_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/4_t_1_S58_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/4_t_2_S59_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/4_t_3_S60_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/4_u_1_S55_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/4_u_2_S56_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/4_u_3_S57_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/8_t_1_S64_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/8_t_2_S65_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/8_t_3_S66_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/8_u_1_S61_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/8_u_2_S62_R1_001.fastq.gz
+curl --netrc -O ftps://ftp.box.com/AGLG06/8_u_3_S63_R1_001.fastq.gz
+
